78e258ab280d97d7be27beb3089530eeed075c2f
Travis McMahon <azgoalie@gmail.com>
2021-12-04 00:53:04 -0700

    Day 1 solutions

---
+++ src/day1.clj
@@ -0,0 +1,24 @@
+(require '[clojure.java.io :as io])
+
+(defn read-input [filename]
+  (with-open [rdr (io/reader filename)]
+    (doall (line-seq rdr))))
+
+(def input (read-input "src/inputs/day1"))
+
+(def part-1
+  (->> input
+       (map #(Integer/parseInt %))
+       (partition 2 1)
+       (filter (fn [[a b]] (< a b)))
+       count))
+       
+
+(def part-2
+  (->> input
+       (map #(Integer/parseInt %))
+       (partition 3 1)
+       (map #(apply + %))
+       (partition 2 1)
+       (filter (fn [[a b]] (< a b)))
+       count))
