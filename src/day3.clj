5f36b3e411e679075652c6581985f88bc09be4d5
Travis McMahon <azgoalie@gmail.com>
2021-12-05 16:28:09 -0700

    Formatted day 3

--- src/day3.clj
+++ src/day3.clj
@@ -27,36 +27,34 @@
       (str (apply str (repeat (- mask-size (count negated)) "0")) negated)
       negated)))
 
-(def gamma (->dec (find-common-bits input)))
-
-(def epsilon
-  (->dec (negate-bits gamma (count (first input)))))
-
-(def part-1 (* gamma epsilon))
+(def part-1
+  (let [gamma (->dec (find-common-bits input))
+        epsilon (->dec (negate-bits gamma (count (first input))))]
+    (* gamma epsilon)))
 
 (defn filter-max-bits
   ([bits]
    (filter-max-bits bits (find-common-bits bits) 0))
   ([bits bit-filter n]
-  (let [filtered-bits (filter #(= (nth bit-filter n) (nth % n)) bits)
-        new-filter (find-common-bits filtered-bits)
-        new-n (inc n)]
-    (if (= 1 (count filtered-bits))
-      (first filtered-bits)
-      (recur filtered-bits new-filter new-n)))))
+   (let [filtered-bits (filter #(= (nth bit-filter n) (nth % n)) bits)
+         new-filter (find-common-bits filtered-bits)
+         new-n (inc n)]
+     (if (= 1 (count filtered-bits))
+       (first filtered-bits)
+       (recur filtered-bits new-filter new-n)))))
 
 (defn filter-min-bits
   ([bits]
    (filter-min-bits bits (negate-bits (->dec (find-common-bits bits)) (count (first bits))) 0))
   ([bits bit-filter n]
-  (let [filtered-bits (filter #(= (nth bit-filter n) (nth % n)) bits)
-        new-filter (negate-bits (->dec (find-common-bits filtered-bits)) (count (first bits)))
-        new-n (inc n)]
-    (if (= 1 (count filtered-bits))
-      (first filtered-bits)
-      (recur filtered-bits new-filter new-n)))))
+   (let [filtered-bits (filter #(= (nth bit-filter n) (nth % n)) bits)
+         new-filter (negate-bits (->dec (find-common-bits filtered-bits)) (count (first bits)))
+         new-n (inc n)]
+     (if (= 1 (count filtered-bits))
+       (first filtered-bits)
+       (recur filtered-bits new-filter new-n)))))
 
 (def part-2
   (let [oxygen-rating (->dec (filter-max-bits input))
         co2-rating (->dec (filter-min-bits input))]
-   (* oxygen-rating co2-rating)))
+    (* oxygen-rating co2-rating)))
